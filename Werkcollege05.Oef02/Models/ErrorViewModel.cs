b7e58ec8c97b4b478268ca22c1bac8d4b711ca68
Wim Hambrouck <wim.hambrouck@ehb.be>
2020-06-11 18:33:18 +0200

    oef2 project

---
+++ Werkcollege05.Oef02/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Werkcollege05.Oef02.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
